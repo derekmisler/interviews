b4b86c5074d6a7438bcee002157c72c0500ecf17
Derek Misler <derekmisler@gmail.com>
2024-01-09 10:35:55 -0500

    savepoint

---
+++ docker/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+} satisfies Config;
